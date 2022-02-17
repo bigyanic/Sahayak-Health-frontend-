0e74a78e3d38bf174ad088d7dc38b93e803348ae
bigyanic <sbigyan9@gmail.com>
2022-02-17 22:42:52 +0545

    Regex Fixed and added

--- src/components/SignupLogin/CreateUser.js
+++ src/components/SignupLogin/CreateUser.js
@@ -4,6 +4,7 @@ import {
   EnvelopeClosedIcon,
   LockClosedIcon,
   ChatBubbleIcon,
+  CalendarIcon,
 } from "@modulz/radix-icons";
 import {
   TextInput,
@@ -12,6 +13,7 @@ import {
   Checkbox,
   Button,
   Paper,
+  Card,
   Text,
   LoadingOverlay,
   Anchor,
@@ -41,7 +43,7 @@ export function AuthenticationForm({ noShadow, noPadding, noSubmit, style }) {
       firstName: "",
       lastName: "",
       email: "",
-      password: "",
+      pswrd: "",
       confirmPassword: "",
       address: "",
       phonenumber: "",
@@ -52,15 +54,20 @@ export function AuthenticationForm({ noShadow, noPadding, noSubmit, style }) {
       firstName: (value) => formType === "login" || value.trim().length >= 2,
       lastName: (value) => formType === "login" || value.trim().length >= 2,
       email: (value) => /^\S+@\S+$/.test(value),
-      password: (value) => /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(value),
-      confirmPassword: (val, values) =>
-        formType === "login" || val === values.password,
+      pswrd: (value) =>
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+          value
+        ),
+      phonenumber: (value) => /(?:\+977[- ])?\d{2}-?\d{7,8}/.test(value),
+      confirmPassword: (val, value) =>
+        formType === "login" || val === value.pswrd,
     },
 
     errorMessages: {
       email: "Invalid email",
-      password:
-        "Password should contain 1 number, 1 letter and at least 6 characters",
+      phonenumber: "Invalid Phonenumber",
+      pswrd:
+        "Password should contain minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character",
       confirmPassword: "Passwords don't match. Try again",
     },
   });
@@ -79,127 +86,132 @@ export function AuthenticationForm({ noShadow, noPadding, noSubmit, style }) {
   };
 
   return (
-    <Paper
-      padding={noPadding ? 0 : "lg"}
-      shadow={noShadow ? null : "sm"}
-      style={{
-        position: "relative",
-        backgroundColor:
-          theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-        ...style,
-      }}
-    >
-      <form onSubmit={form.onSubmit(handleSubmit)}>
-        <LoadingOverlay visible={loading} />
-        {formType === "register" && (
-          <Group grow>
-            <TextInput
-              data-autofocus
-              required
-              placeholder="Your first name"
-              label="First name"
-              {...form.getInputProps("firstName")}
-            />
+    <div style={{ width: 500, margin: "auto" }}>
+      <Card shadow="sm" padding="lg">
+        <Paper
+          padding={noPadding ? 0 : "lg"}
+          shadow={noShadow ? null : "sm"}
+          style={{
+            position: "relative",
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+            ...style,
+          }}
+        >
+          <form onSubmit={form.onSubmit(handleSubmit)}>
+            <LoadingOverlay visible={loading} />
+            {formType === "register" && (
+              <Group grow>
+                <TextInput
+                  data-autofocus
+                  required
+                  placeholder="Your first name"
+                  label="First name"
+                  {...form.getInputProps("firstName")}
+                />
+
+                <TextInput
+                  required
+                  placeholder="Your last name"
+                  label="Last name"
+                  {...form.getInputProps("lastName")}
+                />
+              </Group>
+            )}
 
             <TextInput
+              mt="md"
               required
-              placeholder="Your last name"
-              label="Last name"
-              {...form.getInputProps("lastName")}
+              placeholder="Your email"
+              label="Email"
+              icon={<EnvelopeClosedIcon />}
+              {...form.getInputProps("email")}
             />
-          </Group>
-        )}
 
-        <TextInput
-          mt="md"
-          required
-          placeholder="Your email"
-          label="Email"
-          icon={<EnvelopeClosedIcon />}
-          {...form.getInputProps("email")}
-        />
+            <PasswordInput
+              mt="md"
+              required
+              placeholder="Password"
+              label="Password"
+              icon={<LockClosedIcon />}
+              {...form.getInputProps("pswrd")}
+            />
 
-        <PasswordInput
-          mt="md"
-          required
-          placeholder="Password"
-          label="Password"
-          icon={<LockClosedIcon />}
-          {...form.getInputProps("password")}
-        />
+            {formType === "register" && (
+              <PasswordInput
+                mt="md"
+                required
+                label="Confirm Password"
+                placeholder="Confirm password"
+                icon={<LockClosedIcon />}
+                {...form.getInputProps("confirmPassword")}
+              />
+            )}
+            {formType === "register" && (
+              <TextInput
+                mt="md"
+                required
+                placeholder="Address"
+                label="Address"
+                icon={<EnvelopeClosedIcon />}
+                {...form.getInputProps("address")}
+              />
+            )}
+            {formType === "register" && (
+              <DatePicker
+                mt="md"
+                placeholder="Pick date"
+                icon={<CalendarIcon />}
+                label="Date of Birth"
+                required
+              />
+            )}
+            {formType === "register" && (
+              <TextInput
+                mt="md"
+                required
+                placeholder="Contact Number"
+                label="Contact Number"
+                icon={<ChatBubbleIcon />}
+                {...form.getInputProps("phonenumber")}
+              />
+            )}
+            {formType === "register" && (
+              <Checkbox
+                mt="xl"
+                label="I agree to sell my soul and privacy to this corporation"
+                {...form.getInputProps("termsOfService", { type: "checkbox" })}
+              />
+            )}
 
-        {formType === "register" && (
-          <PasswordInput
-            mt="md"
-            required
-            label="Confirm Password"
-            placeholder="Confirm password"
-            icon={<LockClosedIcon />}
-            {...form.getInputProps("confirmPassword")}
-          />
-        )}
-        {formType === "register" && (
-          <TextInput
-            mt="md"
-            required
-            placeholder="Address"
-            label="Address"
-            icon={<EnvelopeClosedIcon />}
-            {...form.getInputProps("address")}
-          />
-        )}
-        {formType === "register" && (
-          <DatePicker
-            mt="md"
-            placeholder="Pick date"
-            label="Date of Birth"
-            required
-          />
-        )}
-        {formType === "register" && (
-          <TextInput
-            mt="md"
-            required
-            placeholder="Contact Number"
-            label="Contact Number"
-            icon={<ChatBubbleIcon />}
-            {...form.getInputProps("contactnumber")}
-          />
-        )}
-        {formType === "register" && (
-          <Checkbox
-            mt="xl"
-            label="I agree to sell my soul and privacy to this corporation"
-            {...form.getInputProps("termsOfService", { type: "checkbox" })}
-          />
-        )}
+            {error && (
+              <Text color="red" size="sm" mt="sm">
+                {error}
+              </Text>
+            )}
 
-        {error && (
-          <Text color="red" size="sm" mt="sm">
-            {error}
-          </Text>
-        )}
+            {!noSubmit && (
+              <Group position="apart" mt="xl">
+                <Anchor
+                  component="button"
+                  type="button"
+                  color="gray"
+                  onClick={toggleFormType}
+                  size="sm"
+                >
+                  {formType === "register"
+                    ? "Have an account? Login"
+                    : "Don't have an account? Register"}
+                </Anchor>
 
-        {!noSubmit && (
-          <Group position="apart" mt="xl">
-            <Anchor
-              component="button"
-              type="button"
-              color="gray"
-              onClick={toggleFormType}
-              size="sm"
-            >
-              {formType === "register"
-                ? "Have an account? Login"
-                : "Don't have an account? Register"}
-            </Anchor>
-
-            <Button color="blue" type="submit">
-              {formType === "register" ? "Register" : "Login"}
-            </Button>
-          </Group>
-        )}
-      </form>
-    </Paper>
+                <Button color="blue" type="submit">
+                  {formType === "register" ? "Register" : "Login"}
+                </Button>
+              </Group>
+            )}
+          </form>
+        </Paper>
+      </Card>
+    </div>
   );
 }
